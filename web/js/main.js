36dd543f522ae719a4c4806ac68e234703d6cce1
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-11-09 21:45:51 +0100

    Push

--- web/js/main.js
+++ web/js/main.js
@@ -1,11 +1,13 @@
 // Fonction pour l'animation de défilement fluide (smooth scrolling)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
     });
 });
 
